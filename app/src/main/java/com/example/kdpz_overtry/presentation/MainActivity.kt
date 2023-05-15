e27d4bfb57173e4cb664d767b53a8a08913c87e7
ebede2g <108631764+ebede2g@users.noreply.github.com>
2023-05-15 19:06:01 +0300

    v1.8 bugs bugs bugs

---
+++ app/src/main/java/com/example/kdpz_overtry/presentation/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kdpz_overtry.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kdpz_overtry.R
+
+class MainActivity : AppCompatActivity() {
+    //lateinit var bindingClass : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+    }
+}
